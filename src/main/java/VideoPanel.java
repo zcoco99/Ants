a4eaf6383a3116ce5a06fca1d08b2321178175c3
yv17 <yitengvoon0511@gmail.com>
2019-12-05 18:18:21 +0000

    done scaling images

--- src/main/java/VideoPanel.java
+++ src/main/java/VideoPanel.java
@@ -3,47 +3,37 @@ import java.awt.*;
 import java.awt.image.BufferedImage;
 import java.awt.image.ImageObserver;
 import java.util.Objects;
+import java.util.TimerTask;
 
 public class VideoPanel extends JPanel {
-    private static int frameID;
+    private static int frameID =0;
     private ImageIcon pic;
     private static JLabel picLabel;
 
     public VideoPanel(){
         setBackground(Color.black);
-        setPreferredSize(new Dimension(700,500));
+        setPreferredSize(new Dimension(200,100));
         setLayout(new BorderLayout());
 
-        /*GridBagConstraints c = new GridBagConstraints();
-        c.fill = GridBagConstraints.BOTH;
-        c.gridx = 0;
-        c.gridy = 0;*/
+        new java.util.Timer().schedule(new TimerTask(){
+            @Override
+            public void run() {
+                //System.out.println("Executed...");
+                removeAll();
+                loadFrame();
+            }
+        }, 500,500);
 
-        //Initializing file name of frames
-        frameID=1;
-        String file_name=String.format("%05d",frameID);
-        String path="./vid_1/" + file_name + ".png";
 
-        System.out.println(getHeight());
-        //System.out.println(getBounds().height);
-
-        ImageIcon pic = new ImageIcon(Objects.requireNonNull(getClass().getClassLoader().getResource(path)));
-        Image img = pic.getImage();//.getScaledInstance((int)getPreferredSize().getWidth(),(int)getPreferredSize().getHeight(),Image.SCALE_SMOOTH);
-        //BufferedImage bi = new BufferedImage(img.getWidth(this),img.getHeight(this),BufferedImage.TYPE_INT_ARGB);
-        //Graphics g = bi.createGraphics();
-        //g.drawImage(img,0,0,this);
-        ImageIcon scaledImage = new ImageIcon(img);
-        picLabel = new JLabel(scaledImage);
-        //picLabel.setSize(getWidth(),getHeight());
-        add(picLabel);
     }
     private void pathName() {
         String file_name=String.format("%05d",frameID);
         String path="./vid_1/" + file_name + ".png";
         System.out.println(path);
         ImageIcon pic = new ImageIcon(Objects.requireNonNull(getClass().getClassLoader().getResource(path)));
-        System.out.println(getHeight());
-        Image img = pic.getImage().getScaledInstance(getWidth(),getHeight(),Image.SCALE_SMOOTH);
+        //System.out.println(getHeight());
+        //System.out.println(getWidth());
+        Image img = pic.getImage().getScaledInstance(getWidth(),getHeight(),Image.SCALE_DEFAULT);
         ImageIcon scaledImage = new ImageIcon(img);
         picLabel = new JLabel(scaledImage);
         add(picLabel);
@@ -51,17 +41,24 @@ public class VideoPanel extends JPanel {
         repaint();
     }
 
+    private void loadFrame(){
+        if(frameID==0) {
+            frameID = 1;
+        }
+        pathName();
+    }
     public void getPrevFrame(){
         remove(picLabel);
         if(frameID>50) {
             frameID = frameID - 50;
         }
-        pathName();
+        //pathName();
     }
 
     public void getNextFrame(){
         remove(picLabel);
-        frameID=frameID+50;
-        pathName();
+        if(frameID<451) {
+            frameID = frameID + 50;
+        }
     };
 }
